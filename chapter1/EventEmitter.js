896cbbaa5025bd4bb9ffcec97c8e6fc1dfa28fd9
Tyler Dinner <ttante@me.com>
2014-06-07 13:21:23 -0400

    cleanup

--- chapter1/EventEmitter.js
+++ chapter1/EventEmitter.js
@@ -25,25 +25,6 @@
 
 
 
-// Create a Readable stream and pipe any data pushed into this stream to process.stdout
-
-
- var Readable = require('stream').Readable;
- var readable = new Readable;
- var count = 0;
-
- readable._read = function() {
- 	if(++Count > 10) {
- 		return readable.push(null);
- 	}
- 	setTimeout(function() {
- 		readable.push(count + "\n");
- 	}, 500);
-  };
-
-  readable.pipe(process.stdout);
-
-
 
 
 
